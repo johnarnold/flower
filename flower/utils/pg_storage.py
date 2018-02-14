52793fc13d6507d0525e61ea141e4b535d163eac
John Arnold (AZURE) <johnar@microsoft.com>
2018-02-14 00:51:43 +0000

    Integrate retries, move event data from jsonb to text

--- flower/utils/pg_storage.py
+++ flower/utils/pg_storage.py
@@ -1,12 +1,16 @@
 import json
 import logging
 from datetime import datetime
+from socket import error as socketerror
 
 import pg8000
 
 logger = logging.getLogger(__name__)
 connection = None
+_connection_options = {}
 skip_callback = False
+
+REQ_MAX_RETRIES = 2
 
 _all_tables = """
 SELECT * FROM information_schema.tables
@@ -19,7 +23,7 @@ _schema = (
         id SERIAL PRIMARY KEY,
         time TIMESTAMP NOT NULL,
         uuid VARCHAR(36) NOT NULL,
-        data JSONB NOT NULL,
+        data TEXT NOT NULL,
         unique (time, data)
     )""",
     "CREATE INDEX event_time_index ON events (time ASC)",
@@ -65,27 +69,49 @@ def event_callback(state, event):
         return
 
     cursor = connection.cursor()
-    try:
-        cursor.execute(_add_event, (
-            datetime.fromtimestamp(event['timestamp']),
-            event['uuid'],
-            json.dumps(event)
-        ))
-        connection.commit()
-    except Exception:
-        connection.rollback()
-        raise
-    finally:
-        cursor.close()
+    for attempt in range(0, REQ_MAX_RETRIES):
+        try:
+            cursor.execute(_add_event, (
+                datetime.fromtimestamp(event['timestamp']),
+                event['uuid'],
+                json.dumps(event)
+            ))
+            connection.commit()
+            logger.debug('Added event {} to PostGreSQL persistence store'.format(event['uuid']))
+        except (socketerror, pg8000.InterfaceError):
+            if attempt < REQ_MAX_RETRIES:
+                logger.warning('Flower encountered a connection error with PostGreSQL database. Retrying.')
+                open_connection(**_connection_options)
+                cursor = connection.cursor()
+                continue
+            else:
+                logger.exception('Flower encountered a connection error with PostGreSQL database. Unable to retry.')
+        except Exception:
+            connection.rollback()
+            raise
+        finally:
+            cursor.close()
+            return
 
 
 def open_connection(user, password, database, host, port, use_ssl):
     global connection
+    global _connection_options
     connection = pg8000.connect(
         user=user, password=password, database=database,
         host=host, port=port, ssl=use_ssl
     )
+    _connection_options = {'user': user,
+                           'password': password,
+                           'database': database,
+                           'host': host,
+                           'port': port,
+                           'use_ssl': use_ssl
+                           }
 
+
+def maybe_create_schema():
+    global connection
     # Create schema if table is missing
     cursor = connection.cursor()
     try:
@@ -112,17 +138,28 @@ def close_connection():
 def get_events(max_events, max_tasks):
     logger.debug('Events loading from postgresql persistence backend')
     cursor = connection.cursor()
-    try:
-        if max_events:
-            if max_events == -1:
-                query = _get_max_events.format(max_events='ALL')
+    retries_remaining = REQ_MAX_RETRIES
+    for attempt in range(0, REQ_MAX_RETRIES):
+        try:
+            if max_events:
+                if max_events == -1:
+                    query = _get_max_events.format(max_events='ALL')
+                else:
+                    query = _get_max_events.format(max_events=max_events)
             else:
-                query = _get_max_events.format(max_events=max_events)
-        else:
-            query = _get_task_events.format(max_tasks=max_tasks)
-        cursor.execute(query)
-        for row in cursor:
-            yield row[0]
-        logger.debug('{} Events loaded from postgresql persistence backend'.format(cursor.rowcount))
-    finally:
-        cursor.close()
+                query = _get_task_events.format(max_tasks=max_tasks)
+            cursor.execute(query)
+            for row in cursor:
+                yield row[0]
+            logger.debug('{} Events loaded from postgresql persistence backend'.format(cursor.rowcount))
+        except (socketerror, pg8000.InterfaceError):
+            if attempt < REQ_MAX_RETRIES:
+                logger.warning('Flower encountered a connection error with PostGreSQL database. Retrying.')
+                open_connection(**_connection_options)
+                cursor = connection.cursor()
+                continue
+            else:
+                logger.exception('Flower encountered a connection error with PostGreSQL database. Unable to retry.')
+        finally:
+            cursor.close()
+            return
